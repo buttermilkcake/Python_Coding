d8c9ef0c262e2dd4191c27dbc3aed824b41ec376
Julie Stark <cakelp21@gmail.com>
2016-11-08 14:43:31 -0600

    Add files via upload

---
+++ Python_Coding/ch_4_ex_4_13_buffet.py
@@ -0,0 +1,9 @@
+foods = ('mac and cheese', 'meatballs', 'jello salad', 'scalloped potatoes', 'cake')
+print('Here are the foods on the buffet menu')
+for food in foods[0:5]:
+    print(food)
+
+foods = ('mac and cheese', 'meatballs', 'caprese salad', 'sweet potatoes', 'pie')
+print("\nHere is the new buffet menu")
+for food in foods[0:5]:
+      print(food)
