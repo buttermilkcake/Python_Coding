fcc2c9751d7ab091099663fa5cc0f5fdbda24373
Julie Stark <cakelp21@gmail.com>
2016-11-08 15:37:22 -0600

    Add files via upload

---
+++ Python_Coding/ch_3_flowers_list.py
@@ -0,0 +1,13 @@
+flowers = ['zinnia', 'marigold', 'rose', 'dahlia', 'poppy']
+
+print("The first three items on the list are:")
+for flower in flowers[:3]:
+    print(flower)
+    
+print("Three flowers from the middle of the list are:")
+for flower in flowers[1:4]:
+    print(flower)
+
+print("The last three flowers on the list are:")
+for flower in flowers[-3:]:
+    print(flower)
