efef1cb2d5e23828684caa03bb7a707695aeaae1
Julie Stark <cakelp21@gmail.com>
2016-11-08 14:39:35 -0600

    Add files via upload

---
+++ Python_Coding/ch_7_ex_7_5_movie_tickets.py
@@ -0,0 +1,9 @@
+age = input("What is your age? ")
+age = int(age)
+
+if age <= 3:
+    print("\nYour ticket price is free.")
+elif age >= 12:
+    print("\nYour ticket price is $15.")
+else:
+    print("\nYour ticket price is $10.")      
