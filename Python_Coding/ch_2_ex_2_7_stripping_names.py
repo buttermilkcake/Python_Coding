bb418035110f3f19fca7cb48e6a70326436e9ed6
Julie Stark <cakelp21@gmail.com>
2016-11-09 13:55:22 -0600

    Add files via upload

---
+++ Python_Coding/ch_2_ex_2_7_stripping_names.py
@@ -0,0 +1,15 @@
+name = " button jones "
+print(name)
+print("\tbutton\tjones")
+print("\nbutton\njones")
+print("\n\tbutton\n\tjones")
+
+name = " button jones "
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+favorite_language = "' python '"
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
